e293ab0f210762c0fb7d50ab7a12c309bd9d74ed
LemonNekoGH <self@lemonneko.moe>
2023-04-02 15:28:55 +0000

    [AUTO] Bump easi-gen version to v0.0.5

---
+++ Formula/easi-gen.rb
@@ -0,0 +1,14 @@
+class EasiGen < Formula
+  desc "Help users to generate code for easier contract interaction"
+  homepage "https://github.com/LemonNekoGH/easinteraction-for-cadence"
+  url "https://github.com/LemonNekoGH/easinteraction-for-cadence" , :using => :git , :tag => "v0.0.5"
+  license "MIT"
+  
+  
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X 'main.Version=#{version}'"), "./cmd/easi-gen/main.go"
+  end
+end
