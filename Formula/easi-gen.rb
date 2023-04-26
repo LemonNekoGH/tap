abffd17af1abc012cd9bfe4f79b1b337ffa1f6d7
LemonNekoGH <self@lemonneko.moe>
2023-04-26 17:43:18 +0000

    [AUTO] Bump easi-gen version to v0.0.17

--- Formula/easi-gen.rb
+++ Formula/easi-gen.rb
@@ -1,7 +1,7 @@
 class EasiGen < Formula
   desc "Help users to generate code for easier contract interaction"
   homepage "https://github.com/LemonNekoGH/easinteraction-for-cadence"
-  url "https://github.com/LemonNekoGH/easinteraction-for-cadence" , :using => :git , :tag => "v0.0.16"
+  url "https://github.com/LemonNekoGH/easinteraction-for-cadence" , :using => :git , :tag => "v0.0.17"
   license "MIT"
   
   
